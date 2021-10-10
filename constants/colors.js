bf2d88a01d76abb43c1fd5892ff5a45edfbca8f0
Kelvin Graddick <kelvingraddick@kg.codes>
2021-10-10 00:35:17 -0400

    First commit after setup

---
+++ constants/colors.js
@@ -0,0 +1,14 @@
+const Colors = {
+  WHITE: 'white',
+  HIGH_LIGHT: 'rgba(255, 255, 255, 0.05)',
+  LIGHT_GRAY: '#dfdfdf',
+  GRAY: 'gray',
+  DARK_GRAY: '#4f4f4f',
+  GREEN: '#00a79d',
+  LIGHT_BLUE: '#0b75c1',
+  MEDIUM_BLUE: '#114b79',
+  DARK_BLUE: '#213068',
+  RED: '#ff4f5a'
+};
+
+export default Colors;
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,59 @@
-import Head from 'next/head'
+import Layout from '../components/layout';
+import Colors from '../constants/colors';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <Layout>
+      <div className="px-4 pt-1 sm:px-0">
+        <div className="bg-local bg-center bg-cover rounded-lg mb-1 h-96" style={{ backgroundImage: "url('../images/background-1.jpg')" }}></div>
+        <div className="form rounded-lg p-6 h-auto">
+          <div className="text-center text-2xl text-white font-semibold">
+            Welcome to DOCme!
+          </div>
+          <div className="text-center text-xl text-white">
+            Tell us what you need below
+          </div>
+          <div className="py-6">
+            <div>
+              <label htmlFor="speciality" className="sr-only">
+                Speciality
+              </label>
+              <select
+                id="speciality"
+                name="Speciality"
+                className="form-select mt-1 block w-full pl-3 pr-10 py-2 text-white focus:outline-none focus:ring-white focus:border-white sm:text-sm rounded-md"
+                defaultValue="Dentist"
+              >
+                <option>Dentist</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="location" className="sr-only">
+                Location
+              </label>
+              <select
+                id="location"
+                name="Location"
+                className="form-select mt-2 block w-full pl-3 pr-10 py-2 text-white focus:outline-none focus:ring-white focus:border-white sm:text-sm rounded-md"
+                defaultValue="New York, NY, USA"
+              >
+                <option>Atlanta, GA, USA</option>
+                <option>New York, NY, USA</option>
+              </select>
+            </div>
+          </div>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
+        <style jsx>{`
+          .form {
+            background-color: ${Colors.LIGHT_BLUE};
+          }
+          .form-select {
+            background-color: ${Colors.HIGH_LIGHT};
+            border: none;
+            color: ${Colors.DARK_BLUE};
+          }
+        `}</style>
+      </div>
+    </Layout>
   )
 }
