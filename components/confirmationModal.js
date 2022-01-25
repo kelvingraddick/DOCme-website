2780519cd26b220bcdb65b03aefcf6eaf2279ae5
Kelvin Graddick <kelvingraddick@kg.codes>
2022-01-25 03:37:40 -0500

    Confirmation dialog UI fix

--- components/confirmationModal.js
+++ components/confirmationModal.js
@@ -67,7 +67,7 @@ export default function ConfirmationModal(props) {
                 { props.onConfirmButtonPress &&
                   <button
                     type="button"
-                    className={classNames('bg-' + props.confirmButtonColor, 'mt-3 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm')}
+                    className={classNames('bg-' + props.confirmButtonColor, 'w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm')}
                     onClick={props.onConfirmButtonPress}
                   >
                     {props.confirmButtonText}
